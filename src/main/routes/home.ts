d7fa0bb175ce518431bc8902366b1c7d972e5684
Frank2025-java <Frankz@iae.nl>
2026-03-04 16:52:56 +0000

    Start dev changes (#1)
    * Windows VSC changes able to start
    * Windows (non-unix) able to commit without issues
    * Remove generated file
    * copilot advice to work with node-modules
    * Copilot advice. Wrong favico version. yarn  node module mixup Express versions
    * git bash on windows need extra slash with openssl arg
    * readme updated
    * run yarn install before cichecks
    * cichecks on formatting
    * cichecks test:a11y

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,14 +1,16 @@
-import { Application } from 'express';
 import axios from 'axios';
+import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
       // An example of connecting to the backend (a starting point)
       const response = await axios.get('http://localhost:4000/get-example-case');
+      // eslint-disable-next-line no-console
       console.log(response.data);
-      res.render('home', { "example": response.data });
+      res.render('home', { example: response.data });
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error('Error making request:', error);
       res.render('home', {});
     }
